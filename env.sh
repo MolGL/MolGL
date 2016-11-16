9956f1ac4d78cfa9a09c9e305e935793399bfe7b
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-16 23:08:02 +0530

    Initial commit
    env.sh, shell script to --/inform/-- environments of "MolGL®" in one go.
    Usage: $ sh env.sh

---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# ==========================++== MolGL® ==++==========================
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
