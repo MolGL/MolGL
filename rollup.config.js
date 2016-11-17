dac6278483672717030ce61a565d2d709d58d2f0
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-18 01:55:49 +0530

    Initial commit
    SEED™: Build System — MolGL®.
    A Sequømics Product — http://sequomics.com/.

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+/*!
+ * MolGL®
+ * Scalable molecular visualisation with WebGL.
+ * ___________________________________________________________________________
+ *
+ * Rollup, http://rollupjs.org/ — A next-generation ES6 module bundler.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 18-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Rollup
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of ES6 Build System/-- of "MolGL®".
+ * ___________________________________________________________________________
+ */
