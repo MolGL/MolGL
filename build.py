77ac216ab8710694eabc29a0f2eac612c359f981
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-18 01:28:20 +0530

    Initial commit
    Copyright © 2014 - 2016, Sequømics Research, All rights reserved.
    Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.

--- build.py
+++ build.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# ————————————————————————————————————————————————————————————————————————————
+# Copyright © 2014 - 2016, Sequømics Research, All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ————————————————————————————————————————————————————————————————————————————
+# Licensed under the Apache License (the "License, version 2.0");
+# you may not use this file except in compliance with the License.
+# ————————————————————————————————————————————————————————————————————————————
