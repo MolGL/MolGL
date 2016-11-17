88ee5b7694a598ba03fd93f2da9c33f62c7dc538
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-18 01:34:02 +0530

    Initial commit
    --/The Heart of Beauty/-- of "MolGL®".

--- config.rb
+++ config.rb
@@ -1,1 +1,21 @@
 #!/usr/bin/env ruby
+
+
+# MolGL®
+# -----------------------------------------
+# From the Desk of Sequømics Corporation.
+#     With ♥ — by Prabhat Kumar.
+# -----------------------------------------
+# @author     : Prabhat Kumar.
+# @copyright  : Sequømics Corporation.
+# @homepage   : http://sequomics.com/.
+# @require    : Ruby
+# @require    : Sass
+# @require    : Compass
+# @version    : 1.0.0
+# @build      : Umeå
+# @date       : 18-Nov-2016
+# @license    : Apache, version 2.0
+# -----------------------------------------
+# Principle   : Standard Module Definition
+# -----------------------------------------
