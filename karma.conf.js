78b267b76c00dabc459c3d8c3028dd7f01a64748
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-18 02:15:09 +0530

    Initial commit
    SEED™: Build + Test System — MolGL®.
    A Sequømics Product — http://sequomics.com/.

--- karma.conf.js
+++ karma.conf.js
@@ -1,1 +1,31 @@
 #!/usr/bin/env node
+
+
+/*!
+ * MolGL®
+ * Scalable molecular visualisation with WebGL.
+ * ___________________________________________________________________________
+ *
+ * karma, https://github.com/karma-runner/karma — The JavaScript Test Runner.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 18-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : karma
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "MolGL®".
+ * ___________________________________________________________________________
+ */
