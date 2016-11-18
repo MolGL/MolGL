603f3cb376630dfa0481c35fc48530e01e4e396b
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-18 13:20:29 +0530

    Initial commit
    The Node.js® — File System.
    What it is? — File I/O is provided by simple wrappers around standard POSIX functions.

--- version.js
+++ version.js
@@ -28,3 +28,7 @@
 // Invoking strict mode.
 ///@purpose: Strict mode applies to entire scripts or to individual functions.
 "use strict";
+
+// To load required Node module.
+///-----------------------------
+var fs          = require('fs');
