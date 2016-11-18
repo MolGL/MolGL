2caea0420d01f290d6612f34393b1c465d52125d
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-11-18 08:08:01 +0530

    Initial commit
    SEED™: Build Version-ing System — MolGL®.
    A Sequømics Product — http://sequomics.com/.

---
+++ version.js
@@ -0,0 +1,26 @@
+/*!
+ * MolGL®
+ * Scalable molecular visualisation with WebGL.
+ * ___________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * ___________________________________________________________________________
+ *
+ * @date      : 18-Nov-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Git
+ * @require   : package.json and bower.json
+ * @build     : SEED™ — Umeå
+ *              └────── A Sequømics Product — http://sequomics.com/.
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Version-ing System/-- of "MolGL®".
+ * ___________________________________________________________________________
+ */
