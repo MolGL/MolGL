dfb0abc4b4fcfd6a34c6cf2f7ffab52238c42ec1
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2016-12-01 20:05:19 +0530

    Initial commit
    Usage: $ node -v | $ npm -v | $ grunt -version

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,3 +29,8 @@
  * --/The Heart of Build System/-- of "MolGL®".
  * ___________________________________________________________________________
  */
+
+
+// # Usage: $ node -v
+// # Usage: $ npm -v
+// # Usage: $ grunt -version
